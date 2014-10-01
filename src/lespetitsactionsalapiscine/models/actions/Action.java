df8b696896a2437d52838648326d1816dd221e0b
Armand (Tydax) Bour <tydaxx@gmail.com>
2014-10-01 11:04:26 +0200

    Initialised ActionState enum and Action class.

---
+++ src/lespetitsactionsalapiscine/models/actions/Action.java
@@ -0,0 +1,15 @@
+package lespetitsactionsalapiscine.models.actions;
+
+/**
+ * Action class describes an action which progresses as we use the 
+ * @author Armand BOUR
+ *
+ */
+
+public abstract class Action {
+	protected ActionState mState;
+	
+	public void doStep() {
+		
+	}
+}
---
+++ src/lespetitsactionsalapiscine/models/actions/ActionState.java
@@ -0,0 +1,11 @@
+package lespetitsactionsalapiscine.models.actions;
+
+/**
+ * Enum describing the different states of an {@link Action}.
+ * @author Armand BOUR
+ *
+ */
+
+public enum ActionState {
+	READY, IN_PROGRESS, FINISHED
+}
