26543ce3c02d9b3c51baaeaa7723f1081892db74
Armand (Tydax) Bour <tydaxx@gmail.com>
2014-10-16 00:26:05 +0200

    Corrected a few types and added full java documentation for all classes.

--- src/lespetitesactionsalapiscine/models/resources/Basket.java
+++ src/lespetitesactionsalapiscine/models/resources/Basket.java
@@ -1,10 +1,15 @@
 package lespetitesactionsalapiscine.models.resources;
 
+/**
+ * This class represents a basket containing clothes.
+ * @author Armand BOUR
+ * @author Antoine PETIT
+ *
+ */
 public class Basket implements Resource {
 
 	@Override
 	public String description() {
-		return "Bascket containing clothes";
+		return "Basket containing clothes";
 	}
-
 }
--- src/lespetitesactionsalapiscine/models/resources/ResourcePool.java
+++ src/lespetitesactionsalapiscine/models/resources/ResourcePool.java
@@ -4,19 +4,24 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 /**
- * The ResourcePool class describe a pool containing some Resources which can be take from this pool and then free it back to the pool    
+ * The ResourcePool class describes a pool containing some Resources which can be taken
+ *  from it and then freed back to the pool. 
  * 
  * @author Armand BOUR
  * @author Antoine PETIT
  */
 public abstract class ResourcePool<R extends Resource> {
 
-	/** The list of the resources which can be take from the pool */
+	/** The list of the resources which can be taken from the pool. */
 	protected LinkedList<R> mAvailableRes;
 	
-	/** The list of the resources which are borrow at the current time */
+	/** The list of the resources which are borrowed at the current time. */
 	protected LinkedList<R> mProvidedRes;
 
+	/**
+	 * Constructor taking one parameter. Creates a ResourcePool with the specified number of resources.
+	 * @param nbResources The number of resources to initialise in the pool.
+	 */
 	public ResourcePool(int nbResources) {
 		mAvailableRes = new LinkedList<R>();
 		mProvidedRes = new LinkedList<R>();
@@ -25,12 +30,16 @@ public abstract class ResourcePool<R extends Resource> {
 		}
 	}
 
+	/**
+	 * Template method to create the specified type of Resource.
+	 * @return A new Resource.
+	 */
 	protected abstract R createResource();
 
 	/**
-	 * Provide a resource in the pool if there is at least one resource in the pool
-	 * @return The Resource if possible 
-	 * @throws NoSuchElementException
+	 * Provides a resource from the pool if at least one exists.
+	 * @return A resource.
+	 * @throws NoSuchElementException when no resource can be provided.
 	 */
 	public R provideResource() throws NoSuchElementException {
 		R rs;
@@ -43,9 +52,9 @@ public abstract class ResourcePool<R extends Resource> {
 	}
 
 	/**
-	 * Free a resource which was borrow from the pool, if not throw the exception
-	 * @param resource the resource which who want to free back in the pool
-	 * @throws IllegalArgumentException
+	 * Frees a resource which was borrowed from the pool.
+	 * @param resource The freed resource that should be retrieved.
+	 * @throws IllegalArgumentException when the resource was not provided by this pool.
 	 */
 	public void freeResource(R resource) throws IllegalArgumentException {
 		if (mProvidedRes.remove(resource)) {
